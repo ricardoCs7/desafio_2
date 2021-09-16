d9271e3d77d69ddf6baeff33f7eb6f64c576915a
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-09-15 23:37:18 -0300

    Signed-off-by: ricardoCs7 <r.carrasco05@ufromail.cl>

---
+++ main.js
@@ -0,0 +1,4 @@
+var num = document.getElementById("btnNum");
+var sec1 = document.getElementById("seccion1");
+var sec2 = document.getElementById("seccion2");
+var sec3 = document.getElementById("seccion3");
