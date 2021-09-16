fce3d922ab07700729d7ede23b7ab9a4e5f2a0e7
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-09-15 23:40:42 -0300

    aaaa

--- main.js
+++ main.js
@@ -2,5 +2,3 @@ var num = document.getElementById("btnNum");
 var sec1 = document.getElementById("seccion1");
 var sec2 = document.getElementById("seccion2");
 var sec3 = document.getElementById("seccion3");
-
-aaaa
